73d1ee7772fe17a122b55d4d6c268dcd9c22bf7c
chloemcgarry <47299403+chloemcgarry@users.noreply.github.com>
2019-03-19 19:49:03 +0000

    Add files via upload

--- problem1.py
+++ problem1.py
@@ -2,8 +2,10 @@
 
 i = int(input("Enter a positive integer"))
 
-sum > 0
-for i > 0
-sum = sum + i
+range(1, i+1)
+# all the numbers between 1 and i+1 are listed
+
+sum = range(1,i+1)
+# add all the numbers from 1 to i together
 
 print(sum)
