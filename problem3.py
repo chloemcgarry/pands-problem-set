3520b14d0ffa4ef0781eee9d0c136e0b82feacc9
chloemcgarry <47299403+chloemcgarry@users.noreply.github.com>
2019-03-20 21:01:07 +0000

    Add files via upload

--- problem3.py
+++ problem3.py
@@ -1,9 +1,10 @@
 #Solution to problem 3
 
-for i in range(1000,10001):
-#for all numbers from 1000 to 10000
-
-    for ( i % 6 == 0), ( i % 12! ==0):
+for x in range(1000, 10001):
+# for all numbers in the range (1000,10001)
+# range will be from 1000 to 10000 inclusive
+    
+    if x % 6 == 0 or x % 12 != 0:
+        print (x)
 # print numbers that are divisible by 6 but not by 12
 
-        print(i)
