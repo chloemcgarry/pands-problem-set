b01f68a19f911824d5d232765155f5a70676e821
chloemcgarry <47299403+chloemcgarry@users.noreply.github.com>
2019-03-26 19:40:49 +0000

    first attempt

---
+++ problem9.py
@@ -0,0 +1,12 @@
+# Solution to problem 9
+
+open(lyrics.py) as f:
+lines = f.readline()
+
+for l in lines:
+    if l == '\n':
+        line.remove(l)
+
+
+for i in range(0, len(lines), 2)
+print(lines[i])
