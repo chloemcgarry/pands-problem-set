94264801320cb830720ae45ed22ae94844f08d74
chloemcgarry <47299403+chloemcgarry@users.noreply.github.com>
2019-03-27 22:59:40 +0000

    2nd Attempt - few changes, no comments

--- problem10.py
+++ problem10.py
@@ -2,9 +2,25 @@
 
 import matplotlib.pyplot as plt 
 import numpy as np
+import matplotlib.colors as colour
 
-x = np.arange(0, 4, 1)
 
-y1 = x
-y2 = x ** 2
-y3 = 2 ** x
+x = np.arange(0.0, 4.0, 0.01)
+
+y_1 = x
+y_2 = x ** 2
+y_3 = 2 ** x
+
+plt.plot([0, 1, 2, 3, 4], colour = "red")
+plt.plot([0, 1, 4, 9,16], colour = "blue")
+plt.plot([1, 2, 4, 8, 16], colour = "green")
+
+
+plt.xlabel("X")
+plt.ylabel("Y")
+
+plt.title("Plot of Functions - y = x, y = x ** 2, y = 2 ** x")
+
+plt.savefig("Plot of Functions")
+
+plt.show()
