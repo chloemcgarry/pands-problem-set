c916b6f05119e25d56f3760766279010218f214a
chloemcgarry <47299403+chloemcgarry@users.noreply.github.com>
2019-03-29 18:29:13 +0000

    Problem 5 solution with comments

--- problem5.py
+++ problem5.py
@@ -1,16 +1,21 @@
 # Solution to problem 5
-# Reference:https://www.programiz.com/python-programming/examples/prime-number
 
+# ASking user to input a positive integer. Assigning the it x
 x = int(input("Enter a positive integer"))
 
 if x > 1:
     for i in range(2, x):
         #checking for factors of the input
         if (x % i) == 0:
+
+#Prints x isn't a prime then break the loop once a factor of i is found.
             print(x, "is not a prime number")
             print(x, "times", x //i, "is", x)
         break
+
+
     else: 
-        print(x, "is a prime number")
+# Prints x is a prime number if no factors are found
+    print(x, "is a prime number")
 else:
     print(x, "is not a prime number")
