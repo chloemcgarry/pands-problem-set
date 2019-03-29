d995f65b01b20c04d3743cc507a3fe801b9e4d80
chloemcgarry <47299403+chloemcgarry@users.noreply.github.com>
2019-03-29 18:31:18 +0000

    Problem 10 solution with comments

--- problem10.py
+++ problem10.py
@@ -1,26 +1,39 @@
 #Solution to Problem 10
 
+#import pyplot from matplotlib
+#Import numpy from matplotlib
+
 import matplotlib.pyplot as plt 
 import numpy as np
 import matplotlib.colors as colour
 
-
-x = np.arange(0.0, 4.0, 0.01)
-
+# Assigning x values
+x = (0, 4, 1)
+#Functions to be plotted
 y_1 = x
 y_2 = x ** 2
 y_3 = 2 ** x
 
-plt.plot([0, 1, 2, 3, 4], colour = "red")
-plt.plot([0, 1, 4, 9,16], colour = "blue")
-plt.plot([1, 2, 4, 8, 16], colour = "green")
 
+#Plot of functions 
+y_1 = plt.plot(0., 4., 0.1)
+y_2 = plt.plot(0., 4., 0.1)
+y_3 = plt.plot(0., 4., 0.1)
 
+#Plotting functions using colour coded symbols
+plt.plot(x, x, 'r--', x, x ** 2, 'bs', x, 2 ** x, 'g^')
+
+#Axis labels
 plt.xlabel("X")
 plt.ylabel("Y")
 
+#Display grid
+plt.grid(True)
+
+#Giving title to plot
 plt.title("Plot of Functions - y = x, y = x ** 2, y = 2 ** x")
 
+#Saving plot of functions
 plt.savefig("Plot of Functions")
 
 plt.show()
