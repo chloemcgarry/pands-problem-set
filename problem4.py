3fc5c2ba06a4339a1bb738c68c000c61960aca6e
chloemcgarry <47299403+chloemcgarry@users.noreply.github.com>
2019-03-29 17:35:01 +0000

    Problem 4 solution with comments

--- problem4.py
+++ problem4.py
@@ -1,13 +1,18 @@
 #solution to problem 4
 
 x = int(input("Please enter a positive integer"))
-
+# User asked to input a positive integer and assigned to x
 while x != 1:
+
+#Divide x by 2 if even.
     if x % 2 == 0:
         x = x // 2
 
         print (x)
 
-        else:
-            x = x * 3 + 1
-            print (x)
+else:
+#Muliplying x by 3 and adding 1 if odd.
+    x = x * 3 + 1
+    
+    print (x)
+#Printing the value of x.
