4b23d926ed14cfec31d2247875a05a0c07bbfd85
chloemcgarry <47299403+chloemcgarry@users.noreply.github.com>
2019-03-29 17:02:27 +0000

    Solution to problem 2 - some comments added

--- problem2.py
+++ problem2.py
@@ -1,10 +1,39 @@
-import datetime
-d = datetime.datetime.today().weekday()
-print ("Enter the day number - 0 is Sunday, 1 is Monday etc.")
+#Solution to Problem 2
 
-if d == 2 or d == 4 :
+import datetime
+#Imported date/time
+
+weekday = datetime.datetime.today().weekday()
+
+if weekday == 0:
+    # Numbering the weekdays
+    weekday_name = "Monday"
+    # Then assigning names to the numbers.
+
+elif weekday == 1:
+    weekday_name = "Tuesday"
+elif weekday == 2:
+    weekday_name = "Wednesday"
+elif weekday == 3:
+    weekday_name = "Thursday"
+elif weekday == 4:
+    weekday_name = "Friday"
+elif weekday == 5:
+    weekday_name = "Saturday"
+elif weekday == 6:
+    weekend_name = "Sunday"
+    print(weekday_name)
+
+if weekday == 2 or weekday == 4:
+    print ("Today begins with the letter T")
+else:
+    print("Today doesn't begin with the letter T")
+
+if datetime.datetime.today().weekday() == 2 or datetime.datetime.today().weekday() == 4:
+#If the day is either Tuesday or Thursday then it will print that "Today begins with the letter T"
+
     print ("Today begins with the letter T")
     
-    if d == 0 or d == 1 or d == 3 or d == 5 or d == 6 :
-    
-        print("Today doesn't begin with the letter T")
+else:
+    print("Today doesn't begin with the letter T")
+#Else is used if the day is not either a Tuesday or a Thursday
